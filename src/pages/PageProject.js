dfc34da2fb79ae7b6ff04ad6a7acb04a0257046c
GodsDogSled <gabekelly@hotmail.ca>
2023-06-09 12:22:08 -0700

    github link opens a new tab

--- src/pages/PageProject.js
+++ src/pages/PageProject.js
@@ -90,7 +90,7 @@ const PageProject = () => {
 
                     <div className="github">
                       <h3>GitHub Repository</h3>
-                      <a href={`${thisProjectData.acf.github_link}`}>Checkout my commits<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="20" height="15" viewBox="0 0 100 85"><path d="M14.727 30 10 34.707 42.942 67.07c3.906 3.902 10.234 3.91 14.141 0l26.25-25.69v11.954H90v-20A3.338 3.338 0 0 0 86.667 30l-20 .003v6.663H78.62L52.37 62.359a3.335 3.335 0 0 1-4.714 0L14.727 30z" /></svg></a>
+                      <a href={`${thisProjectData.acf.github_link}`} target="_blank" rel="noreferrer">Checkout my commits<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="20" height="15" viewBox="0 0 100 85"><path d="M14.727 30 10 34.707 42.942 67.07c3.906 3.902 10.234 3.91 14.141 0l26.25-25.69v11.954H90v-20A3.338 3.338 0 0 0 86.667 30l-20 .003v6.663H78.62L52.37 62.359a3.335 3.335 0 0 1-4.714 0L14.727 30z" /></svg></a>
                     </div>
                   </div>
                 </section>
